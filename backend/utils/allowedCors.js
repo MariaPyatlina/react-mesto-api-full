e9965d299c2229ec433137672b1ccfa101dd42c9
LD\mpavlovna <mpavlovna@yandex-team.ru>
2023-02-16 01:14:39 +0300

    PR15: fix object

--- backend/utils/allowedCors.js
+++ backend/utils/allowedCors.js
@@ -1,8 +1,10 @@
-const allowedCors = [
-  'https://mesto.for.students.nomoredomainsclub.ru',
-  'http://mesto.for.students.nomoredomainsclub.ru',
-  'localhost:3000',
-  'localhost:3001',
-];
+const allowedCors = {
+  origin: [
+    'https://mesto.for.students.nomoredomainsclub.ru',
+    'http://mesto.for.students.nomoredomainsclub.ru',
+    'localhost:3000',
+    'localhost:3001',
+  ],
+};
 
 module.exports = { allowedCors };
