e3dfe53da4f246f5ec7ff6e03ca8ecb7b4854551
LD\mpavlovna <mpavlovna@yandex-team.ru>
2023-02-19 22:29:44 +0300

    PR15: update regexp

--- backend/utils/regularExpression.js
+++ backend/utils/regularExpression.js
@@ -1,4 +1,4 @@
-const LINK_REGEX = `${/https?:\/\/(www\.)?[a-z0-9-._\-~:/?#[\]@!$&'()*+,;=]+#?/}`;
+const LINK_REGEX = /https?:\/\/(www\.)?[a-z0-9-._\-~:/?#[\]@!$&'()*+,;=]+#?/;
 
 module.exports = {
   LINK_REGEX,
